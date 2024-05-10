c274fc4046bba2fda6873b81ab5f4ca131dc3e90
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-10 18:39:22 +0800

    refactor: add type annotations

--- src/main.py
+++ src/main.py
@@ -8,25 +8,31 @@ counter = 0
 img_size = (250, 250)
 
 
-def main():
-    global counter, interval
-    vid_capture = cv2.VideoCapture(2)
+def main() -> None:
+    """
+    Main function to capture images from webcam and save them to disk
 
-    process_this_frame = True
-
-    last_time = time.time()
+    Returns:
+        None
+    """
+    counter: int = 0
+    interval: float = 2.0
+    last_time: float = time.time()
+    process_this_frame: bool = True
+    vid_capture: cv2.VideoCapture = cv2.VideoCapture(2)
 
     while True:
         _, frame = vid_capture.read()
 
         if process_this_frame:
             # Resize the frame of video to 1/4 size for faster face recognition processing
-            sm_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            sm_frame: np.ndarray = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
             # Convert color from BGR to RGB
-            rgb_frame = sm_frame[:, :, ::-1]
+            rgb_frame: np.ndarray = sm_frame[:, :, ::-1]
 
-            face_locations = face_recognition.face_locations(rgb_frame)
+            face_locations: list[tuple[int, int, int, int]
+                                 ] = face_recognition.face_locations(rgb_frame)
             # face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
         process_this_frame = not process_this_frame
